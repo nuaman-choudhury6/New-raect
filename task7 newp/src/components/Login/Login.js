7e1a94ed7cf2bb0a581aef4a15646e014f4a62fd
nuaman-choudhury6 <nuamanchy66@gmail.com>
2024-03-13 10:52:52 -0700

    task8

--- task7 newp/src/components/Login/Login.js
+++ task7 newp/src/components/Login/Login.js
@@ -12,10 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() =>{
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-  }, [enteredEmail,enteredPassword]);   
+    const identifier = setTimeout(() => {
+      console.log('cehcking form validity!')
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );  
+    }, 500);
+    return () =>{
+      console.log('CLAEANUP!');
+      clearTimeout(identifier);
+    } ;
+  }, [enteredEmail,enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
